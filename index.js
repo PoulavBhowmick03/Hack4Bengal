22cf70fc233319c57c86c4b2ba9ddbf490eae745
Poulav Bhowmick <bpoulav@gmail.com>
2023-07-10 01:31:28 +0530

    major

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 window.addEventListener("scroll", () => {
     document.querySelector("nav").classList.toggle
-    ('window-scroll', window.scrollY > 150)})
+    ('window-scroll', window.scrollY > 10)})
 
 
     //show/hide faqs
