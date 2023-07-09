fb353529a1fd7501c2e8008891cd0153ca3b5500
Poulav Bhowmick <bpoulav@gmail.com>
2023-07-10 01:09:46 +0530

    dropdown

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ window.addEventListener("scroll", () => {
             faq.classList.toggle("open");
         })
     })
+
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener('mouseenter', () => {
+    dropdown.querySelector('.dropdown-menu').style.display = 'block';
+  });
+
+  dropdown.addEventListener('mouseleave', () => {
+    dropdown.querySelector('.dropdown-menu').style.display = 'none';
+  });
+});
